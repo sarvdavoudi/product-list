5126e68e319d1908b1eb8e97e2a751fd58636e49
sarvdavoudi <sarvdavoudi@gmai.com>
2024-10-22 09:59:05 +0330

    remove useless comments

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,27 +1,6 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// export const cartSlice = createSlice({
-//   name: "cartSlice",
-//   initialState: { value: 0 },
-//   reducers: {
-//     increment: (state) => {
-//       state.value += 1;
-//     },
-//     decrement: (state) => {
-//       if (state.value > 0) {
-//         state.value -= 1;
-//       }
-//     },
-//   },
-// });
-
-// export const { increment, decrement, incrementByAmount } = cartSlice.actions;
-
-// export default cartSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isCartOpen: false,
   cartItems: [],
 };
 
