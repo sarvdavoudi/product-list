57d7bb0389d8161b4f600bb14ca87d1e97708ccb
sarvdavoudi <sarvdavoudi@gmai.com>
2024-10-26 10:31:07 +0330

    cart updated

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,51 +6,50 @@ export const Modal = ({ closeModal }) => {
     .reduce((prevValue, currValue) => prevValue + currValue, 0);
   return (
     <div className="modal-overlay">
-      <div
-        className="modal"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-        }}
-      >
-        <h2>Order Confirmed</h2>
-        <p>we hope you enjoy your food!</p>
-        {cartItems.map((item) => {
-          return (
-            <div
-              className="cart-item"
-              key={item.id}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <img
-                src={item.image}
-                style={{ width: "4rem", marginRight: "10px" }}
-              />
-              <div style={{ flex: 1 }}>
-                <h5> {item.name}</h5>
-                <div style={{ color: "var(--Red)", fontWeight: "800" }}>
-                  *{item.quantity}
+      <div className="modal">
+        <div
+          className="modalContent"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          <h2>Order Confirmed</h2>
+          <p>we hope you enjoy your food!</p>
+          {cartItems.map((item) => {
+            return (
+              <div
+                className="modal-item"
+                key={item.id}
+                style={{
+                }}
+              >
+                <img
+                  src={item.image}
+                  style={{ width: "4rem" }}
+                />
+                <div style={{ flex: 1 }}>
+                  <h5> {item.name}</h5>
+                  <div style={{ color: "var(--Red)", fontWeight: "800" }}>
+                    *{item.quantity}
+                  </div>
+                </div>
+
+                <div className="modal-item-price">
+                  <p style={{ fontWeight: "800", textAlign: "right" }}>
+                    ${item.price}
+                  </p>
                 </div>
               </div>
-
-              <div className="cart-item-price">
-                <p style={{ fontWeight: "800", textAlign: "right" }}>
-                  ${item.price}
-                </p>
-              </div>
-            </div>
-          );
-        })}
-        <div style={{ display: "flex" }}>
-          <label style={{ flex: 1 }}>Order Total:</label>
-          <h3> ${cartTotal.toLocaleString()}</h3>
+            );
+          })}
+          <div style={{ display: "flex" }}>
+            <label style={{ flex: 1 }}>Order Total:</label>
+            <h3> ${cartTotal.toLocaleString()}</h3>
+          </div>
+          <button onClick={closeModal}>start new order</button>
         </div>
-        <button onClick={closeModal}>start new order</button>
       </div>
     </div>
   );
